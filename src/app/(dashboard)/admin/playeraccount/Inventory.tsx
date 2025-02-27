a8ced66948a1aaac0c49efa04054a27fcdf615f1
Darel Honrejas <133852063+D4rzDev@users.noreply.github.com>
2025-02-27 14:22:57 +0800

    pooosh

--- src/app/(dashboard)/admin/playeraccount/Inventory.tsx
+++ src/app/(dashboard)/admin/playeraccount/Inventory.tsx
@@ -104,7 +104,7 @@ export default function Inventory() {
 
       <div className=' w-full p-6 bg-cream shadow-sm rounded-md'>
 
-        <div className=' flex flex-wrap items-center gap-4 mb-6'>
+        {/* <div className=' flex flex-wrap items-center gap-4 mb-6'>
             <Input type='text' placeholder='Search e.g user123' value={search} onChange={(e) => setSearch(e.target.value)} className=' w-[250px] bg-gray-100 '/>
 
             <Select value={current} onValueChange={setCurrent} >
@@ -120,7 +120,7 @@ export default function Inventory() {
             </Select>
 
 
-        </div>
+        </div> */}
 
 
         <Table>
